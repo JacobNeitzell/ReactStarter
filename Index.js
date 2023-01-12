b9170b1f0838e5184df5ea26728f6d5b89f2960c
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2023-01-12 12:07:40 -0700

    Test

--- Index.js
+++ Index.js
@@ -1,6 +1,1 @@
-// @ts-ignore
 ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
-
-
-// @ts-ignore
-ReactDOM.render(<ul><li>Groceries</li> <li>Car Parts</li> </ul>, document.getElementById("root"))
